73ba96f36fa43dedb039774dbb73f05f9edbb31c
Elio PASTORET <elpastor@bess-f3r4s10.clusters.42paris.fr>
2025-03-18 14:48:09 +0100

    vrm clean

--- train_test_count.sh
+++
@@ -1,6 +0,0 @@
-# Description: This script is used to run logreg_train.py and logreg_predict.py, then calculate the accuracy of the prediction.
-python3 logreg_train.py datasets/dataset_train.csv
-python3 logreg_predict.py datasets/dataset_test.csv
-i=$(diff houses.csv datasets/dataset_truth.csv | wc -l)
-result=$(echo "scale=2;100-$i/16" | bc -l)
-echo $result
